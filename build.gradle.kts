178533178f627326c80b1a2213858443372b70b1
mostofaaly007-jpg <mostofaaly007@gmail.com>
2026-09-15 22:27:11 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,6 @@
 plugins {
-    id("com.google.gms.google-services")
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
-}
-
-android {
-    namespace = "com.gscrool.app"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.gscrool.app"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
-
-dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
-    implementation("io.coil-kt:coil-compose:2.7.0")
-    implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
-    implementation("com.google.firebase:firebase-auth")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
